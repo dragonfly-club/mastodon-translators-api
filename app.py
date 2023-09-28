2b440a4c42c47dbd0196921b3529611cbbf13172
Holger Huo <50446405+HolgerHuo@users.noreply.github.com>
2023-09-28 14:35:56 +0800

    feat: add langdetect module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # license: GPL-3.0 license
 # author: holgerhuo
 
+import os
 from hashlib import md5
 import random
 import re
@@ -10,9 +11,12 @@ from flask import Flask, request
 from flask_redis import FlaskRedis
 
 import translators as ts
+from ftlangdetect import detect
 
 languages = ['de', 'en', 'es', 'fr', 'hi', 'it', 'jp', 'kr', 'pt', 'ru', 'tr', 'vi', 'zh']
 translators = ['google', 'bing', 'lingvanex', 'itranslate', 'reverso', 'papago']
+FORCE_LANG_DETECT = os.getenv('FORCE_LANG_DETECT')
+USE_DETECTED_LANG = os.getenv('USE_DETECTED_LANG')
 
 app = Flask(__name__)
 app.config['REDIS_URL'] = 'redis://:@localhost:6379/0'
@@ -42,7 +46,7 @@ def translate():
     def _translate(content):
         is_html = re.match(r'^<.*>.*<.*>$', content.strip())
         hash = md5(content.encode('utf-8')).hexdigest()
-        key = '{}:to_{}:{}'.format('html' if is_html else 'text', target, hash)
+        key = '{}:{}:to_{}'.format('html' if is_html else 'text', hash, target)
 
         cached_result = rc.hgetall(key)
 
@@ -50,27 +54,36 @@ def translate():
             if target in languages and target != 'auto':
                 translator = random.choice(translators)
                 backend = ts.translate_html if is_html else ts.translate_text
-                result = {'translatedText': backend(content, translator, source, target), 'translator': translator}
+
+                if source == 'auto' or FORCE_LANG_DETECT:
+                    detectedLanguage = detect(text=''.join(re.compile('>([\\s\\S]*?)<').findall(content)))
+                    detectedLanguage = {'confidence': round(detectedLanguage['score']*100, 1), 'language': detectedLanguage['lang']}
+                else:
+                    detectedLanguage = {'confidence': 100.0, 'language': source}
+
+                result = {'translatedText': backend(content, translator, detectedLanguage['language'] if USE_DETECTED_LANG else source, target), 'translator': translator}
+
                 rc.hset(key, mapping=result)
-                return result
+                rc.hset(key+':detected_lang', mapping=detectedLanguage)
+                return {**result, 'detectedLanguage': detectedLanguage}
             else:
                 return {'error': {'code': 400, 'msg': 'Unsupported Language'}}, 400
         else:
-            return cached_result
+            detectedLanguage = rc.hgetall(key+':detected_lang')
+            return {**cached_result, 'detectedLanguage': detectedLanguage}
 
     if isinstance(query, str):
         return _translate(query)
     elif isinstance(query, list):
-        _results = []
-        _translators = []
+        _results = {'translatedText': [], 'translator': [], 'detectedLanguage': []}
         for q in query:
-            _result=_translate(q)
-            _results.append(_result['translatedText'])
-            _translators.append(_result['translator'])
-        return {'translatedText': _results, 'translator': _translators}
+            _result = _translate(q)
+            _results['translatedText'].append(_result['translatedText'])
+            _results['translator'].append(_result['translator'])
+            _results['detectedLanguage'].append(_result['detectedLanguage'])
+        return _results
     else:
         return {'error': {'code': 400, 'msg': 'Invalid Input'}}, 400
-
 
 # languages endpoint: not implemented
 @app.route('/languages')
